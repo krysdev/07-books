58046e39b75f1e28f29a1da09a50f1309746c122
KrysDev <webdevkrys@gmail.com>
2023-02-09 20:45:58 +0000

    x

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,22 @@
 import BookShow from './BookShow';
 
+import { useContext } from 'react';
+import BooksContext from '../context/books';
+
 function BookList({ books, onDelete, onEdit }) {
+  const contextValue = useContext(BooksContext);
+
   const renderedBooks = books.map((el) => {
-    return <BookShow key={el.id} book={el} onDelete={onDelete} onEdit={onEdit}/>;
+    return (
+      <BookShow key={el.id} book={el} onDelete={onDelete} onEdit={onEdit} />
+    );
   });
 
-  return <div className='book-list'>{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {renderedBooks} - {contextValue}
+    </div>
+  );
 }
 
 export default BookList;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import BooksContext from './context/books';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BooksContext.Provider value={5555}>
+      <App />
+    </BooksContext.Provider>
   </React.StrictMode>
 );
